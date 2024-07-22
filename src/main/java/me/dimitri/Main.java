5cfc62f2fb66d425e0c02a2a167533cdb79494ac
Dimitri-Bit <dimitrijefustic0@gmail.com>
2024-07-22 22:03:10 +0200

    serializer refactor

--- src/main/java/me/dimitri/Main.java
+++ src/main/java/me/dimitri/Main.java
@@ -1,33 +1,43 @@
 package me.dimitri;
 
 import me.dimitri.model.Chart;
-import me.dimitri.model.Data;
-import me.dimitri.model.Datasets;
-import me.dimitri.serializer.ChartConverter;
+import me.dimitri.model.ChartData;
+import me.dimitri.model.ChartDatasets;
+import me.dimitri.model.attribute.AdvancedData;
+import me.dimitri.serializer.ChartSerializer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
 
-        Datasets datasets = new Datasets();
-        datasets.setData(new String[]{"70"});
-        datasets.setBackgroundColor("green");
+        AdvancedData d1 = getData(1, 4, 9);
+        AdvancedData d2 = getData(2 ,4 , 6);
+        AdvancedData d3 = getData(3, 8, 30);
+        AdvancedData d4 = getData(0, 10, 1);
+        AdvancedData d5 = getData(10, 5, 5);
 
-        Data data = new Data();
-        data.setDatasets(new Datasets[]{datasets});
+        AdvancedData[] dArr = {d1, d2, d3, d4, d5};
+
+        ChartDatasets datasets = new ChartDatasets();
+        datasets.setLabel("Data 1");
+        datasets.setData(dArr);
+
+        ChartData data = new ChartData();
+        data.setDatasets(new ChartDatasets[]{datasets});
 
         Chart chart = new Chart();
-        chart.setType("radialGauge");
+        chart.setType("bubble");
         chart.setData(data);
 
-        ChartConverter chartConverter = new ChartConverter();
-
-        try {
-            System.out.println(chartConverter.convertToJson(chart));
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-
+        ChartSerializer chartSerializer = new ChartSerializer();
+        System.out.println(chartSerializer.serializeChartObject(chart));
     }
 
+    private static AdvancedData getData(int x, int y, int r) {
+        AdvancedData advancedData = new AdvancedData();
+        advancedData.setX(x);
+        advancedData.setY(y);
+        advancedData.setR(r);
+        return advancedData;
+    }
 }
---
+++ src/main/java/me/dimitri/serializer/ChartSerializer.java
@@ -0,0 +1,83 @@
+package me.dimitri.serializer;
+
+import me.dimitri.exception.InvalidChartException;
+
+import java.lang.reflect.Field;
+
+public class ChartSerializer {
+
+    private static final String PACKAGE_NAME = "me.dimitri.model";
+
+    public String serializeChartObject(Object object) throws IllegalAccessException {
+
+        if (object == null) {
+            throw new NullPointerException("Null chart provided");
+        }
+
+        // Todo: Look into verifying object with interfaces
+        if (!object.getClass().getPackageName().startsWith(PACKAGE_NAME)) {
+            throw new InvalidChartException("Object is not from the valid package");
+        }
+
+        StringBuilder json = new StringBuilder();
+        json.append("{");
+
+        Field[] fields = object.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+
+            Object fieldValue = field.get(object);
+            if (fieldValue == null) {
+                continue;
+            }
+
+            if (json.length() > 1) {
+                json.append(",");
+            }
+
+            String serializedObject = serializeObject(fieldValue);
+            json.append("\"").append(field.getName()).append("\":").append(serializedObject);
+        }
+
+        json.append("}");
+        return json.toString();
+    }
+
+    private String serializeObject(Object value) throws IllegalAccessException {
+        StringBuilder json = new StringBuilder();
+
+        if (value instanceof String) {
+            json.append("\"").append(value).append("\"");
+            return json.toString();
+        }
+
+        if (value instanceof Double || value instanceof Integer || value instanceof Boolean) {
+            json.append(value);
+            return json.toString();
+        }
+
+        if (value.getClass().isArray()) {
+            json.append(serializeArray(value));
+            return json.toString();
+        }
+
+        return serializeChartObject(value);
+    }
+
+    private String serializeArray(Object object) throws IllegalAccessException {
+        StringBuilder json = new StringBuilder();
+        json.append("[");
+
+        Object[] arr = (Object[]) object;
+        for (int i = 0; i < arr.length; i++) {
+            json.append(serializeObject(arr[i]));
+
+            if (i < (arr.length - 1)) {
+                json.append(",");
+            }
+        }
+
+        json.append("]");
+        return json.toString();
+    }
+}
