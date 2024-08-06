ede811ca2b368417023ff9c3841ef82676266b17
Dimitri-Bit <dimitrijefustic0@gmail.com>
2024-08-06 21:35:03 +0200

    additional dataset attributes

--- src/main/java/me/dimitri/model/chart/impl/ChartDataSetImpl.java
+++ src/main/java/me/dimitri/model/chart/impl/ChartDataSetImpl.java
@@ -11,20 +11,36 @@ import me.dimitri.model.chart.DataPoint;
 @Setter
 public class ChartDataSetImpl implements ChartDataSet {
 
-    private String type;
-    private String label;
+
     private String backgroundColor;
+    private String borderCapStyle;
     private String borderColor;
-    private String color;
-    private String yAxisID;
-    private String xAxisID;
-    private Boolean fill;
-    private Boolean hidden;
+    private Number[] borderDash;
+    private Number borderDashOffset;
+    private String borderJoinStyle;
     private Number borderWidth;
+    private String cubicInterpolationMode;
+    private Number clip;
+    private Boolean fill;
+    private String label;
     private Number lineTension;
-    private Number value;
+    private Number order;
+    private String pointBackgroundColor;
+    private String pointBorderColor;
+    private Number pointBorderWidth;
+    private Number pointHitRadius;
     private Number pointRadius;
+    private Number pointRotation;
     private String pointStyle;
+    private Boolean showLine;
+    private Boolean spanGaps;
+    private Boolean steppedLine;
+    private String xAxisID;
+    private String yAxisID;
+    private String type;
+    private String color;
+    private Boolean hidden;
+    private Number value;
     private DataPoint[] data;
 
 }
