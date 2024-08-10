a4b89462bdcff756355a11c6e9229575ed29bcf1
Dimitri-Bit <dimitrijefustic0@gmail.com>
2024-08-10 17:26:16 +0200

    small improvement

--- src/main/java/me/dimitri/util/DataPointHelper.java
+++ src/main/java/me/dimitri/util/DataPointHelper.java
@@ -4,30 +4,15 @@ import lombok.experimental.UtilityClass;
 import me.dimitri.model.chart.DataPoint;
 import me.dimitri.model.chart.impl.datapoint.GenericDataPoint;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 @UtilityClass
 public class DataPointHelper {
 
-    public static DataPoint[] dataPoints(Number[] arr) {
-        List<GenericDataPoint<Number>> genericDataPoints = new ArrayList<>();
-
-        for (Number number : arr) {
-            genericDataPoints.add(new GenericDataPoint<>(number));
-        }
-
-        return genericDataPoints.toArray(new DataPoint[0]);
-    }
-
-    public static DataPoint[] dataPoints(String[] arr) {
-        List<GenericDataPoint<String>> genericDataPoints = new ArrayList<>();
-
-        for (String string : arr) {
-            genericDataPoints.add(new GenericDataPoint<>(string));
-        }
-
-        return genericDataPoints.toArray(new DataPoint[0]);
+    public static <T> DataPoint[] dataPoints(T[] arr) {
+        return Arrays.stream(arr)
+                .map(GenericDataPoint::new)
+                .toArray(DataPoint[]::new);
     }
 
 }
